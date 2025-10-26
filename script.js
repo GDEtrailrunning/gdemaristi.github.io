0341bd35bde97d599b2ff8d68675fed774b39f00
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-10-26 14:18:44 -0300

    Subo semana 31 y 32

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 29',
-    data: [10, 21, 16, 12, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 31',
+    data: [17, 9, 13, 9, 36], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 30',
-    data: [15, 23, 12, 14, 29], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 32',
+    data: [12, 15, 14, 14, 14], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -52,27 +52,17 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
 
 
 function actualizarDiasRestantes() {
-  // Obtener el título que contiene la fecha objetivo
-  const pageTitle =  document.getElementById('pageTitle').innerText;
-    
-  // Extraer la fecha de la cadena del título
-  const objetivoDate = new Date(pageTitle.split(": ")[1]);
-
-  // Obtener la fecha actual
+  // Fecha objetivo fija
+  const objetivoDate = new Date('2025-11-20');
   const currentDate = new Date();
-
-  // Calcular la diferencia en milisegundos entre las dos fechas
   const timeDiff = objetivoDate - currentDate;
-
-  // Calcular la cantidad de días
-  const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-
-  // Mostrar el resultado
-  const resultElement = document.getElementById('result');
-  resultElement.textContent = `Faltan ${daysDiff} días para llegar a la carrera.`;
-  console.log("actualizarDiasRestantes function called");
-  }
-
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeDiff / (1000 * 60)) % 60);
+  const seconds = Math.floor((timeDiff / 1000) % 60);
+  const countdownElement = document.getElementById('countdown');
+  countdownElement.innerHTML = `<span class="cuenta-regresiva-titulo">CUENTA REGRESIVA</span><br><span class="cuenta-regresiva-numero">${days}d : ${hours}h : ${minutes}m : ${seconds}s</span>`;
+}
   // Crear dos nuevas instancias de HTMLVideoElement para cada sección
   let Semana_1_video_1 = document.createElement("video");
   let Semana_1_video_2 = document.createElement("video");
