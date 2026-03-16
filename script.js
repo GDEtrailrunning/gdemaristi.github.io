44b53fda60eea23278ea95895018077c7abd8676
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-15 21:32:42 -0300

    Subo 11 y 12

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 9',
-    data: [16, 22, 21, 12, 35], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 11',
+    data: [16, 19, 14, 15, 32], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 10',
-    data: [15, 27, 20, 15, 30], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 12',
+    data: [14, 22, 11, 18, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
