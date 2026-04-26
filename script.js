570d48e4b327d3412521950012b22a31946662f5
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-04-26 20:09:37 -0300

    Subo semana 17 y 18

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
   // Datos para el gráfico de líneas
 const dataLine1 = {
-  labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 15',
-    data: [10, 16, 10], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 17',
+    data: [10, 7, 9, 13, 18], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 16',
-    data:[11, 8, 12, 18, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 18',
+    data:[10, 18, 7, 11, 20],// Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
